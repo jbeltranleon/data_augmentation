0e76718b1575c728cd336b44745a2b9d94bcb2bc
Jhonbeltran <jbeltranleon@gmail.com>
2018-10-15 20:00:04 -0500

    add subfix

--- augmentation.py
+++ augmentation.py
@@ -6,18 +6,23 @@ import cv2
 import numpy as np
 
 class Booster:
-    def __init__(self, folder, extension):
+    def __init__(self, folder, extension, name):
         self.folder = folder
         self.extension = extension
+        self.name = name
 
     #flip
     def flip_image(self, image, dir):
         image = cv2.flip(image, dir)
-        cv2.imwrite(self.folder + '/flip-' + str(dir) + self.extension, image)
+        cv2.imwrite(
+            self.folder + '/{}-flip-'.format(name) + str(dir) + self.extension,
+            image)
 
     def invert_image(self, image, channel):
         image = (channel - image)
-        cv2.imwrite(self.folder + '/invert-' + str(channel) + self.extension, image)
+        cv2.imwrite(
+            self.folder + '/{}-invert-'.format(name) + str(channel) + self.extension,
+            image)
 
     def add_light(self, image, gamma=1.0):
         inv_gamma = 1.0 / gamma
@@ -27,9 +32,13 @@ class Booster:
         image = cv2.LUT(image, table)
 
         if gamma >=1:
-            cv2.imwrite(self.folder + '/ligth-' + str(gamma) + self.extension, image)
+            cv2.imwrite(
+                self.folder + '/{}-ligth-'.format(name) + str(gamma) + self.extension,
+                image)
         else:
-            cv2.imwrite(self.folder + '/dark-' + str(gamma) + self.extension, image)
+            cv2.imwrite(
+                self.folder + '/{}-dark-'.format(name) + str(gamma) + self.extension,
+                image)
 
     def add_light_color(self, image, color, gamma=1.0):
         inv_gamma = 1.0 / gamma
@@ -40,9 +49,9 @@ class Booster:
         image = cv2.LUT(image, table)
 
         if gamma >=1:
-            cv2.imwrite(self.folder+'/ligth_color-' + str(gamma) + self.extension, image)
+            cv2.imwrite(self.folder+'/{}-ligth_color-'.format(name) + str(gamma) + self.extension, image)
         else:
-            cv2.imwrite(self.folder+'/dark_color-' + str(gamma) + self.extension, image)
+            cv2.imwrite(self.folder+'/{}-dark_color-'.format(name) + str(gamma) + self.extension, image)
 
     def saturation_image(self, image, saturation):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -52,7 +61,7 @@ class Booster:
         image[:, :, 2] = v
 
         image == cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        cv2.imwrite(self.folder + '/saturation-' + str(saturation) + self.extension,image)
+        cv2.imwrite(self.folder + '/{}-saturation-'.format(name) + str(saturation) + self.extension,image)
 
     def hue_image(self, image, saturation):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -62,65 +71,65 @@ class Booster:
         image[:, :, 2] = v
 
         image == cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        cv2.imwrite(self.folder + '/hue-' + str(saturation) + self.extension,image)
+        cv2.imwrite(self.folder + '/{}-hue-'.format(name) + str(saturation) + self.extension,image)
 
     def multiply_image(self, image, R,G,B):
         image = image*[R,G,B]
         cv2.imwrite(
-            self.folder+'/multiply-'+str(R)+'*'+str(G)+'*'+str(B)+'*'+ self.extension,
+            self.folder+'/{}-multiply-'.format(name)+str(R)+'*'+str(G)+'*'+str(B)+'*'+ self.extension,
             image)
 
     def gaussian_blur(self, image, blur):
         image = cv2.GaussianBlur(image, (5,5), blur)
-        cv2.imwrite(self.folder+'/gaussian_blur-'+str(blur)+self.extension, image)
+        cv2.imwrite(self.folder+'/{}gaussian_blur-'.format(name)+str(blur)+self.extension, image)
 
     def averageing_blur(self, image, shift):
         image = cv2.blur(image, (shift, shift))
-        cv2.imwrite(self.folder+'/averageing_blur-'+str(shift)+self.extension, image)
+        cv2.imwrite(self.folder+'/{}averageing_blur-'.format(name)+str(shift)+self.extension, image)
 
     def median_blur(self, image, shift):
         image = cv2.medianBlur(image, shift)
-        cv2.imwrite(self.folder+'/median_blur-'+str(shift)+self.extension, image)
+        cv2.imwrite(self.folder+'/{}-median_blur-'.format(name)+str(shift)+self.extension, image)
 
     def bilateral_blur(self, image, d, color, space):
         image = cv2.bilateralFilter(image, d, color, space)
         cv2.imwrite(
-            self.folder+'/bi_blur-'+str(d)+'*'+str(color)+'*'+str(space)+self.extension,
+            self.folder+'/{}-bi_blur-'.format(name)+str(d)+'*'+str(color)+'*'+str(space)+self.extension,
             image)
 
     def erosion_image(self, image, shift):
         kernel = np.ones((shift,shift),np.uint8)
         image = cv2.erode(image, kernel, iterations=1)
-        cv2.imwrite(self.folder+'/erosion-'+str(shift)+self.extension, image)
+        cv2.imwrite(self.folder+'/{}-erosion-'.format(name)+str(shift)+self.extension, image)
 
     def dilatation_image(self, image, shift):
         kernel = np.ones((shift,shift),np.uint8)
         image = cv2.dilate(image, kernel, iterations=1)
-        cv2.imwrite(self.folder+'/dilatation-'+str(shift)+self.extension, image)
+        cv2.imwrite(self.folder+'/{}-dilatation-'.format(name)+str(shift)+self.extension, image)
 
     def opening_image(self, image, shift):
         kernel = np.ones((shift,shift),np.uint8)
         image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-        cv2.imwrite(self.folder+'/opening-'+str(shift)+self.extension, image)
+        cv2.imwrite(self.folder+'/{}-opening-'.format(name)+str(shift)+self.extension, image)
 
     def closing_image(self, image, shift):
         kernel = np.ones((shift,shift),np.uint8)
         image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
-        cv2.imwrite(self.folder+'/closing-'+str(shift)+self.extension, image)
+        cv2.imwrite(self.folder+'/{}-closing-'.format(name)+str(shift)+self.extension, image)
 
     def morphological_gradient_image(self, image, shift):
         kernel = np.ones((shift,shift),np.uint8)
         image = cv2.morphologyEx(image, cv2.MORPH_GRADIENT, kernel)
         cv2.imwrite(
-            self.folder+'/morphological_gradient-'+str(shift)+self.extension,
+            self.folder+'/{}-morphological_gradient-'.format(name)+str(shift)+self.extension,
             image)
 
     def top_hat_image(self, image, shift):
         kernel = np.ones((shift,shift),np.uint8)
         image = cv2.morphologyEx(image, cv2.MORPH_TOPHAT, kernel)
-        cv2.imwrite(self.folder+'/top_hat-'+str(shift)+self.extension, image)
+        cv2.imwrite(self.folder+'/{}-top_hat-'.format(name)+str(shift)+self.extension, image)
 
     def black_hat_image(self, image, shift):
         kernel = np.ones((shift,shift),np.uint8)
         image = cv2.morphologyEx(image, cv2.MORPH_BLACKHAT, kernel)
-        cv2.imwrite(self.folder+'/black_hat-'+str(shift)+self.extension, image)
+        cv2.imwrite(self.folder+'/{}black_hat-'.format(name)+str(shift)+self.extension, image)
